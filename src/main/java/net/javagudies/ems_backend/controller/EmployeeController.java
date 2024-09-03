9624c0efba8eabc28a66d665d6fb3a0a925f1065
unknown <garyshi0921@gmail.com>
2024-09-02 22:42:10 -0400

    solve CORS problem

--- src/main/java/net/javagudies/ems_backend/controller/EmployeeController.java
+++ src/main/java/net/javagudies/ems_backend/controller/EmployeeController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/employees")
 @AllArgsConstructor
+@CrossOrigin("*")
 public class EmployeeController {
     private EmployeeService employeeService;
 
